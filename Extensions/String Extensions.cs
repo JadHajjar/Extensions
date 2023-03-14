46ce41506c86c02beba8288b46f9651491295863
JadHajjar <jad@brains-lb.com>
2023-03-15 00:08:59 +0200

    Update String Extensions.cs

--- Extensions/String Extensions.cs
+++ Extensions/String Extensions.cs
@@ -14,52 +14,78 @@ namespace Extensions
 		public static bool AbbreviationCheck(this string string1, string string2)
 		{
 			if (string.IsNullOrWhiteSpace(string1) || string.IsNullOrWhiteSpace(string1))
+			{
 				return false;
+			}
 
 			string1 = string1.ToLower().Replace("'s ", " ");
 			string2 = string2.ToLower().Replace("'s ", " ");
 
-			return string1.Where(x => x != ' ') == string2.GetAbbreviation() || string1.GetAbbreviation() == string2.Where(x => x != ' ');
+			var abbreviation2 = string2.GetAbbreviation();
+			var abbreviation1 = string1.GetAbbreviation();
+
+			return (abbreviation2.StartsWith(string1.Where(x => x != ' ')) && abbreviation2.Length > 2)
+				|| (abbreviation1.StartsWith(string2.Where(x => x != ' ')) && abbreviation1.Length > 2);
 		}
 
 		public static IEnumerable<string> WhereNotEmpty(this IEnumerable<string> enumerable)
-			=> enumerable.Where(x => !string.IsNullOrWhiteSpace(x));
+		{
+			return enumerable.Where(x => !string.IsNullOrWhiteSpace(x));
+		}
 
 		public static string Plural(this string @base, bool plural, string addition = "s")
-			=> plural ? $"{@base}{addition}" : @base;
+		{
+			return plural ? $"{@base}{addition}" : @base;
+		}
 
 		public static string Plural(this string @base, bool plural, string ifplural, string ifnot)
-			=> plural ? $"{@base}{ifplural}" : $"{@base}{ifnot}";
+		{
+			return plural ? $"{@base}{ifplural}" : $"{@base}{ifnot}";
+		}
 
 		public static string Plural(this string @base, int plural, string addition = "s")
-			=> plural != 1 ? $"{@base}{addition}" : @base;
+		{
+			return plural != 1 ? $"{@base}{addition}" : @base;
+		}
 
 		public static string Plural(this string @base, int plural, string ifplural, string ifnot)
-			=> plural != 1 ? $"{@base}{ifplural}" : $"{@base}{ifnot}";
+		{
+			return plural != 1 ? $"{@base}{ifplural}" : $"{@base}{ifnot}";
+		}
 
 		public static string Plural<T>(this string @base, IEnumerable<T> plural, string addition = "s")
-			=> (plural?.Count() ?? 0) != 1 ? $"{@base}{addition}" : @base;
+		{
+			return (plural?.Count() ?? 0) != 1 ? $"{@base}{addition}" : @base;
+		}
 
 		public static string Plural<T>(this string @base, IEnumerable<T> plural, string ifplural, string ifnot)
-			=> (plural?.Count() ?? 0) != 1 ? $"{@base}{ifplural}" : $"{@base}{ifnot}";
+		{
+			return (plural?.Count() ?? 0) != 1 ? $"{@base}{ifplural}" : $"{@base}{ifnot}";
+		}
 
 		/// <summary>
 		/// Gets what's between the <paramref name="Start"/> <see cref="char"/> and the <paramref name="End"/> <see cref="string"/> in the given <see cref="string"/>
 		/// </summary>
 		public static string Between(this string S, char Start, string End)
-			=> S.Between(Start.ToString(), End);
+		{
+			return S.Between(Start.ToString(), End);
+		}
 
 		/// <summary>
 		/// Gets what's between the <paramref name="Start"/> <see cref="string"/> and the <paramref name="End"/> <see cref="char"/> in the given <see cref="string"/>
 		/// </summary>
 		public static string Between(this string S, string Start, char End)
-			=> S.Between(Start, End.ToString());
+		{
+			return S.Between(Start, End.ToString());
+		}
 
 		/// <summary>
 		/// Gets what's between the <paramref name="Start"/> <see cref="char"/> and the <paramref name="End"/> <see cref="char"/> in the given <see cref="string"/>
 		/// </summary>
 		public static string Between(this string S, char Start, char End)
-			=> S.Between(Start.ToString(), End.ToString());
+		{
+			return S.Between(Start.ToString(), End.ToString());
+		}
 
 		/// <summary>
 		/// Gets what's between the <paramref name="Start"/> <see cref="string"/> and the <paramref name="End"/> <see cref="string"/> in the given <see cref="string"/>
@@ -75,14 +101,18 @@ namespace Extensions
 		/// Checks if the <see cref="string"/> contains a specific Word
 		/// </summary>
 		public static bool ContainsWord(this string S, string Word, bool ignoreCase = false)
-			=> Regex.IsMatch(S, @"\b" + Regex.Escape(Word) + @"\b", ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
+		{
+			return Regex.IsMatch(S, @"\b" + Regex.Escape(Word) + @"\b", ignoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
+		}
 
 		public static string Copy(this string str, int times)
 		{
 			var sb = new StringBuilder(str.Length * times);
 
 			for (var i = 0; i < times; i++)
+			{
 				sb.Append(str);
+			}
 
 			return sb.ToString();
 		}
@@ -94,7 +124,9 @@ namespace Extensions
 		{
 			var SB = new StringBuilder();
 			foreach (var item in S.GetWords(true))
+			{
 				SB.Append(item.All(x => char.IsDigit(x)) ? item : item[0].ToString());
+			}
 
 			if (Regex.IsMatch(SB.ToString(), "^[A-z]+[0-9]+$"))
 			{
@@ -109,8 +141,10 @@ namespace Extensions
 		/// Gets an <see cref="string[]"/> containing all the Words in the <see cref="string"/>
 		/// </summary>
 		public static string[] GetWords(this string S, bool includeNumbers = false)
-			=> (S == string.Empty) ? new string[0] : Regex.Matches(S, @"\b" + (includeNumbers ? "" : "(?![0-9])") + @"(\w+)(?:'\w+)?\b")
-				.Convert(x => x.Groups[1].Value).ToArray();
+		{
+			return (S == string.Empty) ? new string[0] : Regex.Matches(S, @"\b" + (includeNumbers ? "" : "(?![0-9])") + @"(\w+)(?:'\w+)?\b")
+						.Convert(x => x.Groups[1].Value).ToArray();
+		}
 
 		public static string FormatWords(this string str, bool forceUpper = false)
 		{
@@ -118,7 +152,9 @@ namespace Extensions
 				.RegexReplace(@"(\b)(?<!')([a-z])", x => $"{x.Groups[1].Value}{x.Groups[2].Value.ToUpper()}", false);
 
 			if (forceUpper)
+			{
 				str = str.RegexReplace(@"(^[a-z])|(\ [a-z])", x => x.Value.ToUpper());
+			}
 
 			return str;
 		}
@@ -127,19 +163,25 @@ namespace Extensions
 		/// Changes the value of the string to <paramref name="value"/> if the string is null or white space
 		/// </summary>
 		public static string IfEmpty(this string S, string value, string @else = null)
-			=> string.IsNullOrWhiteSpace(S) ? value : (@else ?? S);
+		{
+			return string.IsNullOrWhiteSpace(S) ? value : (@else ?? S);
+		}
 
 		/// <summary>
 		/// Lists all the strings in the <see cref="IEnumerable{string}"/> next to each other in one <see cref="string"/>
 		/// </summary>
 		public static string ListStrings<T>(this IEnumerable<T> list, string seperator)
-			=> list.ListStrings(x => x + seperator, false);
-		
+		{
+			return list.ListStrings(x => x + seperator, false);
+		}
+
 		/// <summary>
 		/// Lists all the strings in the <see cref="IEnumerable{string}"/> next to each other in one <see cref="string"/>
 		/// </summary>
 		public static string ListStrings<T>(this IEnumerable<T> list, Func<T, string> Format, string seperator)
-			=> list.Select(Format).ListStrings(x => x + seperator, false);
+		{
+			return list.Select(Format).ListStrings(x => x + seperator, false);
+		}
 
 		/// <summary>
 		/// Lists all the strings in the <see cref="IEnumerable{string}"/> with a set <paramref name="Format"/>
@@ -148,7 +190,9 @@ namespace Extensions
 		public static string ListStrings<T>(this IEnumerable<T> list, Func<T, string> Format = null, bool applyToLast = true)
 		{
 			if (list == null)
+			{
 				return string.Empty;
+			}
 
 			var SB = new StringBuilder();
 			var arr = list.ToArray();
@@ -156,9 +200,13 @@ namespace Extensions
 			for (var i = 0; i < arr.Length; i++)
 			{
 				if (!applyToLast && i == arr.Length - 1)
+				{
 					SB.Append(arr[i]);
+				}
 				else
+				{
 					SB.Append(Format?.Invoke(arr[i]) ?? arr[i].ToString());
+				}
 			}
 
 			return SB.ToString();
@@ -171,7 +219,9 @@ namespace Extensions
 		/// <param name="group">Specifies which <paramref name="group"/> in the <see cref="Regex"/> match to remove</param>
 		/// <param name="ignoreCase">IgnoreCase <see cref="Regex"/> option</param>
 		public static string RegexRemove(this string @base, string pattern, int group = 0, bool ignoreCase = true)
-			=> @base == null ? null : ignoreCase ? Regex.Replace(@base, pattern, x => x.Value.Remove(x.Groups[group].Value), RegexOptions.IgnoreCase) : Regex.Replace(@base, pattern, x => x.Value.Remove(x.Groups[group].Value), RegexOptions.IgnoreCase);
+		{
+			return @base == null ? null : ignoreCase ? Regex.Replace(@base, pattern, x => x.Value.Remove(x.Groups[group].Value), RegexOptions.IgnoreCase) : Regex.Replace(@base, pattern, x => x.Value.Remove(x.Groups[group].Value), RegexOptions.IgnoreCase);
+		}
 
 		/// <summary>
 		/// Replaces all occurrence of a <see cref="Regex"/> <paramref name="pattern"/> with a <paramref name="replacement"/>
@@ -180,7 +230,9 @@ namespace Extensions
 		/// <param name="replacement">Text Replacement</param>
 		/// <param name="ignoreCase">IgnoreCase <see cref="Regex"/> option</param>
 		public static string RegexReplace(this string @base, string pattern, string replacement = "", bool ignoreCase = true)
-			=> @base == null ? null : ignoreCase ? Regex.Replace(@base, pattern, replacement, RegexOptions.IgnoreCase) : Regex.Replace(@base, pattern, replacement, RegexOptions.IgnoreCase);
+		{
+			return @base == null ? null : ignoreCase ? Regex.Replace(@base, pattern, replacement, RegexOptions.IgnoreCase) : Regex.Replace(@base, pattern, replacement, RegexOptions.IgnoreCase);
+		}
 
 		/// <summary>
 		/// Replaces all occurrence of a <see cref="Regex"/> <paramref name="pattern"/> with the result of the <paramref name="replacement"/> method
@@ -189,38 +241,52 @@ namespace Extensions
 		/// <param name="replacement">Method that converts the matched string</param>
 		/// <param name="ignoreCase">IgnoreCase <see cref="Regex"/> option</param>
 		public static string RegexReplace(this string @base, string pattern, MatchEvaluator replacement = null, bool ignoreCase = true)
-			=> ignoreCase ? Regex.Replace(@base, pattern, replacement, RegexOptions.IgnoreCase) : Regex.Replace(@base, pattern, replacement);
+		{
+			return ignoreCase ? Regex.Replace(@base, pattern, replacement, RegexOptions.IgnoreCase) : Regex.Replace(@base, pattern, replacement);
+		}
 
 		/// <summary>
 		/// Removes the first occurrence of the text in the <see cref="string"/>
 		/// </summary>
 		public static string Remove(this string S, string text)
-			=> S == null ? null : (S.IndexOf(text) >= 0 ? S.Remove(S.IndexOf(text), text.Length) : S);
+		{
+			return S == null ? null : (S.IndexOf(text) >= 0 ? S.Remove(S.IndexOf(text), text.Length) : S);
+		}
 
 		/// <summary>
 		/// Removes the character at the specified index
 		/// </summary>
 		public static string RemoveAt(this string S, int index)
-			=> S.Remove(index, 1);
+		{
+			return S.Remove(index, 1);
+		}
 
 		/// <summary>
 		/// Removes any multiple spaces after each other and any starting or ending spaces
 		/// </summary>
 		public static string RemoveDoubleSpaces(this string S)
-			=> S.RegexReplace(@" {2,}", " ")?.Trim() ?? string.Empty;
+		{
+			return S.RegexReplace(@" {2,}", " ")?.Trim() ?? string.Empty;
+		}
 
 		/// <summary>
 		/// Parses the <see cref="string"/> into an <see cref="int"/> disregarding non-digit characters
 		/// </summary>
 		/// <returns>Returns 0 if no digits are found</returns>
 		public static int SmartParse(this string S, int defaultValue = 0)
-			=> !string.IsNullOrWhiteSpace(S) && (int.TryParse(S.Where(c => char.IsDigit(c)), out var i)) ? i : defaultValue;
+		{
+			return !string.IsNullOrWhiteSpace(S) && int.TryParse(S.Where(c => char.IsDigit(c)), out var i) ? i : defaultValue;
+		}
 
 		public static double SmartParseD(this string S, double defaultValue = 0)
-			=> Regex.Match(S, @"[-+]?[0-9]*[,\.]?[0-9]+([eE][-+]?[0-9]+)?").If(x => x.Success, x => double.TryParse(x.Value, out var d) ? d : defaultValue, defaultValue);
+		{
+			return Regex.Match(S, @"[-+]?[0-9]*[,\.]?[0-9]+([eE][-+]?[0-9]+)?").If(x => x.Success, x => double.TryParse(x.Value, out var d) ? d : defaultValue, defaultValue);
+		}
 
 		public static float SmartParseF(this string S, float defaultValue = 0)
-			=> Regex.Match(S, @"[-+]?[0-9]*[,\.]?[0-9]+([eE][-+]?[0-9]+)?").If(x => x.Success, x => float.TryParse(x.Value, out var f) ? f : defaultValue, defaultValue);
+		{
+			return Regex.Match(S, @"[-+]?[0-9]*[,\.]?[0-9]+([eE][-+]?[0-9]+)?").If(x => x.Success, x => float.TryParse(x.Value, out var f) ? f : defaultValue, defaultValue);
+		}
 
 		/// <summary>
 		/// Parses the <see cref="string"/> into an <see cref="int"/> disregarding non-digit characters
@@ -229,12 +295,16 @@ namespace Extensions
 		public static int SmartParse(this string S, int defaultValue, bool strict)
 		{
 			if (string.IsNullOrEmpty(S))
+			{
 				return defaultValue;
+			}
 
 			var i = 0;
 
 			if (strict)
+			{
 				return int.TryParse(S, out i) ? i : defaultValue;
+			}
 
 			return int.TryParse(S.Where(c => char.IsDigit(c)), out i) ? i : defaultValue;
 		}
@@ -247,7 +317,9 @@ namespace Extensions
 		/// <param name="caseCheck">Option to match the strings with Case Sensitivity</param>
 		/// <returns></returns>
 		public static bool SpellCheck(this string s1, string s2, int maxErrors = 2, bool caseCheck = true)
-			=> SpellCheck(s1, s2, caseCheck) <= maxErrors;
+		{
+			return SpellCheck(s1, s2, caseCheck) <= maxErrors;
+		}
 
 		/// <summary>
 		/// Checks if the <see cref="string"/> matches <paramref name="s2"/> with a <paramref name="MaxErrors"/> margin
@@ -257,9 +329,12 @@ namespace Extensions
 		/// <param name="caseCheck">Option to match the strings with Case Sensitivity</param>
 		/// <returns></returns>
 		public static bool SearchCheck(this string s1, string s2, bool caseCheck = false)
-			=> !string.IsNullOrWhiteSpace(s1) && !string.IsNullOrWhiteSpace(s2) && (SpellCheck(s1, s2, caseCheck) <= (int)Math.Ceiling((s2.Length - 4) / 5M)
-			|| (caseCheck ? s1 : s1.ToLower()).Contains((caseCheck ? s2 : s2.ToLower()))
-			|| (s1.AbbreviationCheck(s2) && s2.GetWords().Length > 2));
+		{
+			return !string.IsNullOrWhiteSpace(s1) && !string.IsNullOrWhiteSpace(s2)
+					&& (SpellCheck(s1, s2, caseCheck) <= (int)Math.Ceiling((s2.Length - 4) / 5M)
+					|| s1.IndexOf(s2, caseCheck ? StringComparison.CurrentCulture : StringComparison.InvariantCultureIgnoreCase) >= 0
+					|| s1.AbbreviationCheck(s2));
+		}
 
 		/// <summary>
 		/// Returns the total amount of differences between the two strings
@@ -283,14 +358,20 @@ namespace Extensions
 			var d = new int[n + 1, m + 1];
 
 			if (n == 0)
+			{
 				return m;
+			}
 
 			if (m == 0)
+			{
 				return n;
+			}
 
-			for (var i = 0; i <= n; d[i, 0] = i++) { }
+			for (var i = 0; i <= n; d[i, 0] = i++)
+			{ }
 
-			for (var j = 0; j <= m; d[0, j] = j++) { }
+			for (var j = 0; j <= m; d[0, j] = j++)
+			{ }
 
 			for (var i = 1; i <= n; i++)
 			{
@@ -310,7 +391,9 @@ namespace Extensions
 		/// </summary>
 		/// <param name="ForceLowerCase">Forces all existing upper-case into lower-case before conversion</param>
 		public static string ToCapital(this string S, bool ForceLowerCase = true)
-			=> Regex.Replace(ForceLowerCase ? S.ToLower() : S, @"\b(([A-z])(\w+)?)", new MatchEvaluator(x => ToUpper(x, S)));
+		{
+			return Regex.Replace(ForceLowerCase ? S.ToLower() : S, @"\b(([A-z])(\w+)?)", new MatchEvaluator(x => ToUpper(x, S)));
+		}
 
 		/// <summary>
 		/// Changes all strings in the <see cref="IEnumerable{string}"/> to Lowercase
@@ -320,7 +403,9 @@ namespace Extensions
 			var array = IE.ToArray();
 
 			for (var i = 0; i < array.Count(); i++)
+			{
 				array[i] = array[i].ToLower();
+			}
 
 			return array.AsEnumerable();
 		}
@@ -329,7 +414,9 @@ namespace Extensions
 		/// Removes <paramref name="count"/> characters from the end of a <see cref="string"/>
 		/// </summary>
 		public static string TrimEnd(this string s, int count)
-																									=> s.Length >= count ? s.Substring(0, s.Length - count) : string.Empty;
+		{
+			return s.Length >= count ? s.Substring(0, s.Length - count) : string.Empty;
+		}
 
 		/// <summary>
 		/// Filters the <see cref="string"/> based on the <paramref name="Test"/> for each <see cref="char"/>
@@ -343,7 +430,9 @@ namespace Extensions
 			foreach (var c in S)
 			{
 				if (Test(c))
+				{
 					Out.Append(c);
+				}
 			}
 
 			return Out.ToString();
@@ -367,11 +456,17 @@ namespace Extensions
 		public static string GetString(this Version version)
 		{
 			if (version.Revision > 0)
+			{
 				return version.ToString(4);
+			}
 			else if (version.Build > 0)
+			{
 				return version.ToString(3);
+			}
 			else
+			{
 				return version.ToString(2);
+			}
 		}
 	}
 }
